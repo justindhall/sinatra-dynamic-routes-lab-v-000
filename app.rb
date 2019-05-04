06246f88be842e6e9c4c3f2e4a67c45fab170f3e
Justin Hall <hall.justin.d@gmail.com>
2019-05-04 20:50:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,4 +12,12 @@ class App < Sinatra::Base
     "#{@number ** 2}"
   end
 
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@number.times do
+          puts @phrase
+        end}"
+  end
+
 end
